f029ad434394c076ea570d3d685dd4d868d744cb
hassey13 <ehasselbring@gmail.com>
2017-04-26 21:05:39 -0400

    basic server

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-chat-component
+
+Socket.io Express Server for Checkers React repo https://github.com/hassey13/Checkers-React
---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+  console.log('New User connected!')
+
+  socket.on('move', function( board ){
+    io.emit('move', board )
+  })
+})
+
+// start the server
+const port = process.env.PORT || 4000;
+const env = process.env.NODE_ENV || 'production';
+server.listen(port, err => {
+  if (err) {
+    return console.error(err);
+  }
+  console.info(`Server running on http://localhost:${port} [${env}]`);
+})
